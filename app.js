fa219a4a09273c6b5fd2aa9d2e7303e8a278e35c
Stella-Long <huierl@nycstudents.net>
2025-11-24 12:32:08 -0500

    ss

---
+++ app.js
@@ -0,0 +1,26 @@
+const pokemonCards = [
+  {
+    name: "Pikachu VMAX (Full Art)",
+    series: "Vivid Voltage",
+    origin: "English",
+    img: "https://tcgplayer-cdn.tcgplayer.com/product/226432_in_1000x1000.jpg",
+  },
+  {
+    name: "Pikachu VMAX (Rainbow Rare Full Art)",
+    series: "Vivid Voltage",
+    origin: "English",
+    img: "https://storage.googleapis.com/images.pricecharting.com/b4253710ee077b36198a6ad560b53302d045026a06588c8aa0b24012ecbc5872/1600.jpg",
+  },
+  {
+    name: "Pikachu VMAX",
+    series: "SWSH Black Star Promos",
+    origin: "English",
+    img: "https://cdn11.bigcommerce.com/s-ua4dd/images/stencil/1280w/products/77093/182556/SWSH286_hires__88323.1683813817.png",
+  },
+  {
+    name: "Pikachu VMAX (Secret Rare)",
+    series: "Lost Origin Trainer Gallery",
+    origin: "English",
+    img: "https://tcgplayer-cdn.tcgplayer.com/product/284302_in_1000x1000.jpg",
+  },
+];
