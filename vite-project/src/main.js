82e837124f590c3656c4c17e373a0f49571451a8
Stella-Long <huierl@nycstudents.net>
2025-12-08 12:32:36 -0500

    s

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -4,49 +4,14 @@ import viteLogo from "/vite.svg";
 
 document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
     <h1>My Pokemon!</h1>
-  <div class="container">
+    <div class="container">
     <class="card">
     </div>
   </div>
 `;
 
 const pokemonCards = [
-  {
-    name: "Pikachu VMAX (Full Art)",
-    character: "Pikachu",
-    series: "Vivid Voltage",
-    origin: "English",
-    img: "https://tcgplayer-cdn.tcgplayer.com/product/226432_in_1000x1000.jpg",
-  },
-  {
-    name: "Pikachu VMAX Rainbow Rare",
-    character: "Pikachu",
-    series: "Vivid Voltage",
-    origin: "English",
-    img: "https://storage.googleapis.com/images.pricecharting.com/b4253710ee077b36198a6ad560b53302d045026a06588c8aa0b24012ecbc5872/1600.jpg",
-  },
-  {
-    name: "Pikachu VMAX (Secret Rare)",
-    character: "Pikachu",
-    series: "Lost Origin Trainer Gallery",
-    origin: "English",
-    img: "https://tcgplayer-cdn.tcgplayer.com/product/284302_in_1000x1000.jpg",
-  },
-  {
-    name: "Pikachu VMAX",
-    character: "Pikachu",
-    series: "SWSH Black Star Promos",
-    origin: "English",
-    img: "https://cdn11.bigcommerce.com/s-ua4dd/images/stencil/1280w/products/77093/182556/SWSH286_hires__88323.1683813817.png",
-  },
-
   {
     name: "Flying Pikachu VMAX",
     character: "Pikachu",
@@ -60,6 +25,34 @@ const pokemonCards = [
     series: "Celebrations",
     origin: "English",
     img: "https://stupidgeeksinc.com/cdn/shop/files/a6c06b63-746e-53eb-bdfa-49b0e52b6431_1000x1000.jpg?v=1733529745",
+  },
+  {
+    name: "Pikachu VMAX (Secret Rare)",
+    character: "Pikachu",
+    series: "Lost Origin Trainer Gallery",
+    origin: "English",
+    img: "https://tcgplayer-cdn.tcgplayer.com/product/284302_in_1000x1000.jpg",
+  },
+  {
+    name: "Pikachu VMAX Rainbow Rare",
+    character: "Pikachu",
+    series: "Vivid Voltage",
+    origin: "English",
+    img: "https://storage.googleapis.com/images.pricecharting.com/b4253710ee077b36198a6ad560b53302d045026a06588c8aa0b24012ecbc5872/1600.jpg",
+  },
+  {
+    name: "Pikachu VMAX (Full Art)",
+    character: "Pikachu",
+    series: "Vivid Voltage",
+    origin: "English",
+    img: "https://tcgplayer-cdn.tcgplayer.com/product/226432_in_1000x1000.jpg",
+  },
+  {
+    name: "Pikachu VMAX",
+    character: "Pikachu",
+    series: "SWSH Black Star Promos",
+    origin: "English",
+    img: "https://cdn11.bigcommerce.com/s-ua4dd/images/stencil/1280w/products/77093/182556/SWSH286_hires__88323.1683813817.png",
   },
   {
     name: "Eevee VMAX",
@@ -113,3 +106,13 @@ document
 document
   .querySelector(".c")
   .addEventListener("click", () => filterByCharacter("Pikachu")); */
+
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
